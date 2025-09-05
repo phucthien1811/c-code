2967fe5a140febca210bc90295f6b563dca7a539
Thien Tiy <jack0baohbocon@gmail.com>
2025-09-05 22:52:15 +0700

    binary Search

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,24 +1,38 @@
-  
-#include <iostream> 
-using namespace std; 
-  
-int search(int arr[], int n, int x) 
-{ 
-    int i; 
-    for (i = 0; i < n; i++) 
-        if (arr[i] == x) 
-            return i; 
-    return -1; 
-} 
-  
-int main(void) 
-{ 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
-    int x = 10; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int result = search(arr, n, x); 
-   (result == -1)? cout<<"Element is not present in array" 
-                 : cout<<"Element is present at index " <<result;
-    cout << endl; 
-   return 0; 
-} 
+#include <iostream>
+
+using namespace std;
+
+bool binary_search(int a[], int n, int x){
+    //Khởi tạo left, right
+    int l = 0, r = n - 1;
+    while(l <= r){
+        //Tính chỉ số của phần tử ở giữa
+        int m = (l + r) / 2;
+        if(a[m] == x){
+            return true; // tìm thấy
+        }
+        else if(a[m] < x){
+            //tìm kiếm ở bên phải
+            l = m + 1;
+        }
+        else{
+            //tìm kiếm ở bên trái
+            r = m - 1;
+        }
+    }
+    return false; // l > r
+}
+
+int main(){
+    int n = 12, x = 24, y = 6;
+    int a[12] = {1, 2, 3, 4, 5, 5, 7, 9, 13, 24, 27, 28};
+    if(binary_search(a, n, x)){
+        cout << "FOUND\n";
+    }
+    else cout << "NOT FOUND\n";
+    if(binary_search(a, n, y)){
+        cout << "FOUND\n";
+    }
+    else cout << "NOT FOUND\n";
+    return 0;
+}
