b7bba4942ba1262ec24df2e7215a7b2ef5987d5c
Thien Tiy <jack0baohbocon@gmail.com>
2025-09-06 10:45:04 +0700

    fibonacy de quy thuan + vong lap

---
+++ Fibonacy.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Đệ quy
+long long fibo_recursive(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibo_recursive(n - 1) + fibo_recursive(n - 2);
+}
+
+// Vòng lặp
+long long fibo_iterative(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    long long f1 = 0, f2 = 1, fn;
+    for (int i = 2; i <= n; i++) {
+        fn = f1 + f2;
+        f1 = f2;
+        f2 = fn;
+    }
+    return f2;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibo_recursive(n) << endl;
+    cout << fibo_iterative(n) << endl;
+    return 0;
+}
