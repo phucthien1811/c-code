d9507ab9771f10e0648d303cf86dd30149b2c09a
Thien Tiy <jack0baohbocon@gmail.com>
2025-09-07 14:44:55 +0700

    jump search - dùng cho mảng đã được sắp xếp

---
+++ jumSearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int jumSearch(int a[],int n, int k){
+    int d = 3;
+    int i = 0;
+    int j = i + d;
+    while ( j < n && a[j] < k){
+        j = j + d;
+        i = i + d;
+    }
+    if (j >= n){
+        j = n - 1;
+    }
+    for (int idx = i ; idx<= j ; idx++){
+        if (a[idx]==k){
+            return idx;
+        }
+   
+        
+}
+return -1;
+}
+
+
+int main() {
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = sizeof(a) / sizeof(a[0]);
+    int k = 10;
+
+    int result = jumSearch(a, n, k);
+    (result == -1) ? cout << "The number not in array ." : cout << "number in array in index: "<< result << endl;
+}
